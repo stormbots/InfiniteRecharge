ae892f401dedd9321b97dc931188c425a1173139
Dan Sheadel <tekdemo@gmail.com>
2020-01-29 02:09:21 -0800

    Add Robot selector code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,5 +15,7 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
+public class Constants {
+    public enum BotName {COMP,PRACTICE,TABI};
+    public static BotName botName = BotName.COMP;
 }
